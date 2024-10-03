b73d6de5a600cd20572d761b43dd373fef2f1a71
Shaik Arshiya <168001057+shaikarshiya-itcbit@users.noreply.github.com>
2024-10-03 15:39:37 +0530

    Create app.jsx

---
+++ 6_084.password_checker/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PasswordStrengthChecker from "./Components/PasswordStrengthChecker"
+import HelloWorld from './Components/Helloworld'
+
+function App() {
+
+  return (
+    <PasswordStrengthChecker />
+  
+  )
+}
+
+export default App
